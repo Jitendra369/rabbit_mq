5feec6c0bbea1c3bafe35a2613c0fb3bbbd9a95e
jitendra <onekb.twotb@gmail.com>
2024-08-10 21:54:41 +0530

    reset commit

--- src/main/java/com/rabbitmq/consumer/RabbitMQConsumer.java
+++ src/main/java/com/rabbitmq/consumer/RabbitMQConsumer.java
@@ -1,5 +1,6 @@
 package com.rabbitmq.consumer;
 
+import com.rabbitmq.config.RabbitMQConfig;
 import com.rabbitmq.dto.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -17,5 +18,15 @@ public class RabbitMQConsumer {
     @RabbitListener(queues = {"${rabbitmq.queue.jsonqueue}"})
     public void consumeJson(User user){
         log.info("getting json message received " + user);
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.MARKETING_QUEUE)
+    public void marketingQueueListner(String message){
+        log.info("message recieved from marketingQueue in Listener "+  message);
+    }
+
+    @RabbitListener(queues = RabbitMQConfig.ADMIN_QUEUE)
+    public void adminQueueListner(String message){
+        log.info("message recieved from AdminQueue Listener "+  message);
     }
 }
